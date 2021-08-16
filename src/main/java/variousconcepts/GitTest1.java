824f9080d477db3830b11c503c8ff27aac209c06
sam <shagere8@gmail.com>
2021-08-16 11:01:20 -0400

    3rd commit

---
+++ src/main/java/variousconcepts/GitTest1.java
@@ -0,0 +1,13 @@
+package variousconcepts;
+
+public class GitTest1 {
+	public static void main(String[] args) {
+		printMe();
+		
+	}
+	public static void printMe() {
+		
+		System.out.println("git test");
+	}
+
+}
